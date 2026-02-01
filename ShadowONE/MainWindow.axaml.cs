38f7579b0c9de02a1b7e4611cb2fe7e60ef776ee
dreamsyntax <dreamsyntax@gmail.com>
2026-01-31 17:34:08 -0700

    Add gitignore and generate AvaloniaUI template

---
+++ ShadowONE/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ShadowONE
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
